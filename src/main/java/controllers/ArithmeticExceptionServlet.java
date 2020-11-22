3b75e56278f41d6ec888a61b53864f41d435363a
bookgvi <bookgvi@gmail.com>
2020-11-22 18:17:47 +0300

    servlet - исключение в отдельном потокеgit add .

--- src/main/java/controllers/ArithmeticExceptionServlet.java
+++ src/main/java/controllers/ArithmeticExceptionServlet.java
@@ -1,16 +1,36 @@
 package controllers;
 
+import javax.annotation.Resource;
+import javax.enterprise.concurrent.ManagedExecutorService;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-@WebServlet(name = "Arithmetic exception source", urlPatterns = "/arithmex")
+@WebServlet(name = "Arithmetic exception source", urlPatterns = "/arithmex", asyncSupported = true)
 public class ArithmeticExceptionServlet extends HttpServlet {
+  @Resource
+  ManagedExecutorService mes;
+
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-    int i = 10 / 0;
+    Integer result = null;
+    try {
+      result = mes.submit(new Callable<Integer>() {
+        public Integer call() {
+          return 10 / 0;
+        }
+      }).get();
+    } catch (InterruptedException | ExecutionException e) {
+      Logger.getLogger("Exception").log(Level.WARNING, e.getMessage());
+    }
+    resp.getWriter().write(result.toString());
   }
 }

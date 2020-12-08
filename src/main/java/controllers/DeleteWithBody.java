e45f1ba29d24de11af2bab6ec06cce1325b162bf
bookgvi <bookgvi@gmail.com>
2020-12-08 18:54:03 +0300

    выбор типа поля при парсинге

--- src/main/java/controllers/DeleteWithBody.java
+++ src/main/java/controllers/DeleteWithBody.java
@@ -12,6 +12,11 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 import java.util.HashMap;
 
+enum Types {
+  string,
+  number
+}
+
 @WebServlet (name = "test delete with body", urlPatterns = "/del")
 public class DeleteWithBody extends HttpServlet {
   private JsonElement jsonElement;
@@ -32,23 +37,26 @@ public class DeleteWithBody extends HttpServlet {
       reqField = "data";
       jsonElement = jsonObject.get(reqField);
       JsonObject data = jsonElement.getAsJsonObject();
-      request.putAll(parseBody_required(stringFields_required, data));
-      request.putAll(parseBody_required(integerFields_required, data));
-      System.out.printf("{ \"name\": \"%s\" } %n", request.get("name"));
-      System.out.printf("{ \"first\": \"%s\" } %n", request.get("first"));
-      System.out.printf("{ \"second\": \"%s\" } %n", request.get("second"));
+      request.putAll(parseBody_required(stringFields_required, data, Types.string));
+      request.putAll(parseBody_required(integerFields_required, data, Types.number));
+      System.out.printf("{ \"name\": \"%s\" } (%s) %n", request.get("name"), request.get("name").getClass().getName());
+      System.out.printf("{ \"first\": %s } (%s) %n", request.get("first"), request.get("first").getClass().getName());
+      System.out.printf("{ \"second\": \"%s\" } (%s) %n", request.get("second"), request.get("second").getClass().getName());
       out.write("OK");
     } catch (NullPointerException ex) {
       throw new NullPointerException("field '" + reqField + "' is required");
     }
   }
 
-  private HashMap<String, Object> parseBody_required(String[] body, JsonObject root) throws NullPointerException {
+  private HashMap<String, Object> parseBody_required(String[] body, JsonObject root, Types type) throws NullPointerException {
     HashMap<String, Object> res = new HashMap<>();
     for (String field: body) {
       reqField = field;
       jsonElement = root.get(reqField);
-      Object requestField = jsonElement.getAsString();
+      Object requestField = null;
+      if (type == Types.string) requestField = jsonElement.getAsString();
+      else if (type == Types.number) requestField = jsonElement.getAsInt();
+
       res.put(reqField, requestField);
     }
     return res;
